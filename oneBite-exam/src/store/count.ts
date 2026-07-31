7b62352e860e0b32b92d7fb3291aceadb46d5501
ImJaePang <narushel@naver.com>
2026-07-31 12:54:59 +0900

    (3.4) Zustand 미들웨어 1 - 1

--- oneBite-exam/src/store/count.ts
+++ oneBite-exam/src/store/count.ts
@@ -1,31 +1,51 @@
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+// type Store = {
+//   count: number;
+//   actions: {
+//     increase: () => void;
+//     decrease: () => void;
+//   };
+// };
+
+export const useCounterStore = create(
+  combine({ count: 0 }, (set, get) => ({
+    actions: {
+      increaseOne: () => {
+        // const count = get().count;
+        // set({ count: count + 1 });
+        set((store) => ({
+          count: store.count + 1,
+        }));
+      },
+      decreaseOne: () => {
+        set((store) => ({
+          count: store.count - 1,
+        }));
+      },
+    },
+  })),
+);
 
 // state, action
-export const useCounterStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      // const count = get().count;
-      // set({ count: count + 1 });
-      set((store) => ({
-        count: store.count + 1,
-      }));
-    },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+// export const useCounterStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => {
+//       // const count = get().count;
+//       // set({ count: count + 1 });
+//       set((store) => ({
+//         count: store.count + 1,
+//       }));
+//     },
+//     decrease: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   const count = useCounterStore((store) => store.count);
@@ -33,11 +53,11 @@ export const useCount = () => {
 };
 
 export const useIncreaseCount = () => {
-  const increase = useCounterStore((store) => store.actions.increase);
+  const increase = useCounterStore((store) => store.actions.increaseOne);
   return increase;
 };
 
 export const useDecreaseCount = () => {
-  const decrease = useCounterStore((store) => store.actions.decrease);
+  const decrease = useCounterStore((store) => store.actions.decreaseOne);
   return decrease;
 };
