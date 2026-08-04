adbc5cf2d44a775e6610cf04a548396d34887f11
ImJaePang <narushel@naver.com>
2026-08-04 12:54:43 +0900

    (3.7) 투두리스트 기능 구현하기 - 1

---
+++ oneBite-exam/src/store/todos.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+import type { Todo } from "../types";
+
+const initailState: {
+  todos: Todo[];
+} = {
+  todos: [],
+};
+
+const useTodoStore = create(
+  immer(
+    combine(initailState, (set) => ({
+      actions: {
+        createTodo: (content: string) => {
+          set((state) => {
+            state.todos.push({ id: new Date().getTime(), content: content });
+          });
+        },
+        deleteTodo: (targetId: number) => {
+          set((state) => {
+            state.todos = state.todos.filter((todo) => todo.id != targetId);
+          });
+        },
+      },
+    })),
+  ),
+);
+
+export const useTodos = () => {
+  const todos = useTodoStore((state) => state.todos);
+};
