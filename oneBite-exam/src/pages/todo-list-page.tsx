9542e6ab474569eba19cca14eb01c088d6f3f1db
ImJaePang <narushel@naver.com>
2026-08-06 12:37:02 +0900

    (4.3) 데이터 조회 요청 관리하기

--- oneBite-exam/src/pages/todo-list-page.tsx
+++ oneBite-exam/src/pages/todo-list-page.tsx
@@ -1,6 +1,6 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
-import { useTodos } from "@/store/todos";
+import { useTodosData } from "@/hooks/queries/use-todos.data";
 
 // const dummyTodos = [
 //   { id: 1, content: "첫 번째 할 일" },
@@ -9,13 +9,18 @@ import { useTodos } from "@/store/todos";
 // ];
 
 export default function TodoList() {
-  const todos = useTodos();
+  // const todos = useTodos();
+
+  const { data: todos, isLoading, error } = useTodosData();
+
+  if (error) return <div>오류가 발생했습니다 </div>;
+  if (isLoading) return <div>로딩중입니다...</div>;
 
   return (
     <div className="flex flex-col gap-5 p-5">
       <h1 className="text-2xl font-bold">Todo List</h1>
       <TodoEditor />
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
     </div>
