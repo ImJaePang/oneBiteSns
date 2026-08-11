a98fe8f4575684fec0de43d58358d042dab742ad
ImJaePang <narushel@naver.com>
2026-08-11 12:50:53 +0900

    (4.4) 캐싱 메커니즘 이해하기 2 - 1

--- oneBite-exam/src/hooks/queries/use-todo-data-by-id.ts
+++ oneBite-exam/src/hooks/queries/use-todo-data-by-id.ts
@@ -5,7 +5,8 @@ export function useTodoDataById(id: number) {
   return useQuery({
     queryFn: () => fetchTodoById(id),
     queryKey: ["todos", id],
-    staleTime: 1000 * 5 * 1, // 5초
+    staleTime: 1000 * 4 * 1, // 5초
+    gcTime: 1000 * 5 * 1, // 5분
   });
   // refetchOnMount: false,
   // refetchInterval: 1000,
