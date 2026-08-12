59f3884d06145fc10e6ee8c492bf47831fc46d79
ImJaePang <narushel@naver.com>
2026-08-12 12:02:24 +0900

    (4.5) 캐싱 메커니즘 이해하기 2 - 캐싱옵션이랑 기본옵션설정도 가능

--- oneBite-exam/src/main.tsx
+++ oneBite-exam/src/main.tsx
@@ -5,7 +5,20 @@ import { BrowserRouter } from "react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0, // 0초
+      gcTime: 1000 * 60 * 5, // 5분
+
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+      // retry: 0,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
