6d76a76c2a08ea0b4e8e6994f00d80487dcf3bf7
ImJaePang <narushel@naver.com>
2026-08-13 12:41:15 +0900

    (4.7) 캐시 데이터 다루기 1 - 데이터 무효화 하기

--- oneBite-exam/src/hooks/mutations/use-create-todo-mutaion.ts
+++ oneBite-exam/src/hooks/mutations/use-create-todo-mutaion.ts
@@ -1,13 +1,21 @@
 import { createTodo } from "@/api/create-todo";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createTodo,
     onMutate: () => {},
     onSettled: () => {},
     onSuccess: () => {
-      window.location.reload();
+      // window.location.reload();
+      queryClient.invalidateQueries({
+        // todo를 새로고침
+        // queryKey: ["todos"],
+        queryKey: QUERY_KEYS.todo.list,
+      });
     },
     onError: (error) => {
       window.alert(error.message);
--- oneBite-exam/src/hooks/queries/use-todo-data-by-id.ts
+++ oneBite-exam/src/hooks/queries/use-todo-data-by-id.ts
@@ -1,10 +1,12 @@
 import { fetchTodoById } from "@/api/fetch-todo-by-id";
+import { QUERY_KEYS } from "@/lib/constants";
 import { useQuery } from "@tanstack/react-query";
 
 export function useTodoDataById(id: string) {
   return useQuery({
     queryFn: () => fetchTodoById(id),
-    queryKey: ["todos", id],
+    // queryKey: ["todos", id],
+    queryKey: QUERY_KEYS.todo.detail(id),
     staleTime: 1000 * 4 * 1, // 5초
     gcTime: 1000 * 5 * 1, // 5분
   });
