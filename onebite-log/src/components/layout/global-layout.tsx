1218c49ea6d369ac1f508050438b168bf0adfd4c
ImJaePang <narushel@naver.com>
2026-08-24 21:38:15 +0900

    (5.4) 글로벌 레이아웃 설정하기

--- onebite-log/src/components/layout/global-layout.tsx
+++ onebite-log/src/components/layout/global-layout.tsx
@@ -5,9 +5,9 @@ import DefaultAvata from "@/assets/default-avatar.png";
 
 export default function GlobalLayout() {
   return (
-    <div>
+    <div className="flex min-h-[100vh] flex-col">
       <header className="h-15 border-b">
-        <div className="flex h-full justify-between">
+        <div className="m-auto flex h-full w-full max-w-175 justify-between px-4">
           <Link to={"/"} className="flex items-center gap-2">
             <img
               className="h-5"
@@ -24,9 +24,12 @@ export default function GlobalLayout() {
           </div>
         </div>
       </header>
-      <main>
+      <main className="m-auto w-full max-w-175 flex-1 border-x px-4 py-6">
         <Outlet />
       </main>
+      <footer className="text-muted-foreground border-t py-10 text-center">
+        @narushel
+      </footer>
     </div>
   );
 }
