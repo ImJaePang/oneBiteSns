fd17f149aa6d8ae0cfb6581683a157727f99a222
ImJaePang <narushel@naver.com>
2026-09-01 12:40:33 +0900

    (6.3) 로그인 구현하기

--- onebite-log/src/pages/sign-in-page.tsx
+++ onebite-log/src/pages/sign-in-page.tsx
@@ -1,3 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-password";
+import { useState } from "react";
+import { Link } from "react-router";
+
 export default function SignInPage() {
-  return <div>SignInPage</div>;
+  const { mutate: signInWithPassword } = useSignInWithPassword();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignInWithPasswordClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+    signInWithPassword({
+      email: email,
+      password: password,
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">로그인</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          className="py-6"
+          type="email"
+          placeholder="exapmle@abc.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          className="py-6"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button className="w-full" onClick={handleSignInWithPasswordClick}>
+          로그인
+        </Button>
+      </div>
+      <div>
+        <Link className="text-muted-foreground hover:underline" to={"/sign-up"}>
+          계정이 없으시다면? 회원가입
+        </Link>
+      </div>
+    </div>
+  );
 }
