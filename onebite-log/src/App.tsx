ec9939737eb938095198e51c30ef6be0cf94b129
ImJaePang <narushel@naver.com>
2026-09-03 12:52:49 +0900

    (6.8) Zustand를 이용해 세션 관리하기 - 1

--- onebite-log/src/App.tsx
+++ onebite-log/src/App.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import supabase from "./lib/supabase";
 import RootRoute from "./root-route";
+import { useSetSession } from "./store/session";
 
 export default function App() {
+  const setSession = useSetSession();
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return <RootRoute />;
 }
