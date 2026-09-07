68ce08042def77e28a7a28a1fda1469d3b692a42
ImJaePang <narushel@naver.com>
2026-09-07 12:52:14 +0900

    (6.10) 비밀번호 재설정 구현하기

---
+++ onebite-log/src/hooks/mutations/use-update-password.ts
@@ -0,0 +1,15 @@
+import { updatePassword } from "@/api/auth";
+import type { UseMutationCallback } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+
+export function useUpdatePassword(callbacks?: UseMutationCallback) {
+  return useMutation({
+    mutationFn: updatePassword,
+    onError: (error) => {
+      if (callbacks?.onError) callbacks.onError(error);
+    },
+    onSuccess: () => {
+      if (callbacks?.onSucess) callbacks.onSucess();
+    },
+  });
+}
